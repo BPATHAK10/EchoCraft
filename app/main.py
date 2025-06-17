0605d36c77215f6ffd52a502698d949cde4b48a8
Biraj Bikram Pathak <birajpathak10@gmail.com>
2025-06-16 19:42:44 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 import sys
+from pathlib import Path
+import os
+import shutil
 
 SHELL_BUILTINS = {
     "echo",
     "exit",
     "type",
 }
-
 
 def main():
     while True:
@@ -19,11 +21,18 @@ def main():
         elif command.startswith("echo"):
             print(command.split(" ", 1)[1] if " " in command else "")
         elif command.startswith("type"):
+            # search in all the path dirs
             builtin = command.split(" ", 1)[1]
+            
             if builtin in SHELL_BUILTINS:
                 print(f"{builtin} is a shell builtin")
+            
+            elif path := shutil.which(builtin):
+                print(f"{builtin} is {path}")
+            
             else:
                 print(f"{builtin}: not found")
+
         else:
             print(f"{command}: command not found")
 
